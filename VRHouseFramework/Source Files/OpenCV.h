77e88e61f07e51cbdf895a7bd3df71099b4299a8
Piotr Sochalewski <piotr.sochalewski@netguru.co>
2018-11-28 12:00:29 +0100

    [UM3D-27] Import Foundation to resolve issues with linking and move imports to prefix header

--- VRHouseFramework/Source Files/OpenCV.h
+++ VRHouseFramework/Source Files/OpenCV.h
@@ -3,14 +3,6 @@
 //  VRHouseFramework
 //
 
-#ifdef __cplusplus
-#undef NO
-#undef YES
-#import <opencv2/opencv.hpp>
-#import <opencv2/core/core.hpp>
-#import <opencv2/imgcodecs/ios.h>
-#endif
-
 NS_ASSUME_NONNULL_BEGIN
 
 @interface OpenCV: NSObject
