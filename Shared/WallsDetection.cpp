5906a2e79b872ee3d12f8bb5e5cb8c1d726b111b
Piotr Sękara <piotr.sekara@gmail.com>
2019-01-28 13:38:04 +0100

    Removed redundant spaces

--- Shared/WallsDetection.cpp
+++ Shared/WallsDetection.cpp
@@ -9,10 +9,10 @@ vector<vector<cv::Point>> WallsDetection::findWalls(cv::Mat input) {
     Mat img = input.clone();
 
     threshold(img, img, 210, 255, THRESH_BINARY);
-    
+
     int an = 2;
-    Mat closeElement = getStructuringElement(MORPH_RECT, Size(an*2+1, an*2+1), Point(an, an) );
-    Mat openElement = getStructuringElement(MORPH_RECT, Size(an*2, an*2), Point(an, an) );
+    Mat closeElement = getStructuringElement(MORPH_RECT, Size(an*2+1, an*2+1), Point(an, an));
+    Mat openElement = getStructuringElement(MORPH_RECT, Size(an*2, an*2), Point(an, an));
     morphologyEx(img, img, MORPH_CLOSE, closeElement);
     morphologyEx(img, img, MORPH_OPEN, openElement);
 
